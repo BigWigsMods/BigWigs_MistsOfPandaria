1e733a3108e9c512bab50be20c2ce6a4d89f15e1
funkydude <funkydude@users.noreply.github.com>
2025-05-18 17:57:11 +0100

    Mogushan/TheStoneGuard: Cleanups

--- Mogushan/TheStoneGuard.lua
+++ Mogushan/TheStoneGuard.lua
@@ -13,7 +13,9 @@ mod:SetRespawnTime(30)
 --
 
 local cobaltTimer = 10.7
+local cobaltCount = 1
 local prevTiles = 0
+local currentPetri = 0
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -65,6 +67,7 @@ end
 function mod:OnBossEnable()
 	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "Petrifications", "boss1", "boss2", "boss3", "boss4")
+	self:Log("SPELL_CAST_SUCCESS", "JasperChains", 130395)
 	self:Log("SPELL_AURA_APPLIED", "JasperChainsApplied", 130395)
 	self:Log("SPELL_AURA_REMOVED", "JasperChainsRemoved", 130395)
 	self:Log("SPELL_AURA_APPLIED", "AmethystPoolDamage", 130774)
@@ -72,11 +75,13 @@ function mod:OnBossEnable()
 	self:Log("SPELL_PERIODIC_MISSED", "AmethystPoolDamage", 130774)
 	self:Log("SPELL_AURA_APPLIED", "EnergizedTilesApplied", 116541)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "EnergizedTilesApplied", 116541)
-	self:RegisterUnitEvent("UNIT_POWER_FREQUENT", nil, "boss1", "boss2", "boss3", "boss4")
+	self:RegisterUnitEvent("UNIT_POWER_UPDATE", nil, "boss1", "boss2", "boss3", "boss4")
 end
 
 function mod:OnEngage()
 	prevTiles = 0
+	cobaltCount = 1
+	currentPetri = 0
 	self:OpenInfo("energy", CL.other:format("BigWigs", CL.energy))
 
 	local diff = self:Difficulty()
@@ -90,44 +95,51 @@ end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, msg, boss, _, _, destName)
 	if msg:find("spell:116529", nil, true) then -- Power Down
+		prevTiles = 0
+		self:SetInfo("energy", 10, "")
 		self:Message(116529, "orange", CL.other:format(self:SpellName(116529), boss))
 		self:PlaySound(116529, "info")
 	end
 end
 
 do
-	local prevPlayerName, prevPlayerGUID, mySaySpamTarget = nil, nil, nil
+	local prevLinkName, prevLinkGUID, mySaySpamTarget = nil, nil, nil
+	function mod:JasperChains(args)
+		prevLinkName, prevLinkGUID, mySaySpamTarget = nil, nil, nil
+		self:Bar(args.spellId, 19.4, CL.link)
+	end
+
 	local function RepeatLinkSay()
 		if not mod:IsEngaged() or not mySaySpamTarget then return end
-		mod:SimpleTimer(RepeatLinkSay, 3)
+		mod:SimpleTimer(RepeatLinkSay, 5)
 		mod:Say(false, CL.link_with:format(mySaySpamTarget), true, ("Linked with %s"):format(mySaySpamTarget))
 	end
+
 	function mod:JasperChainsApplied(args)
-		if not prevPlayerName then
-			prevPlayerName, prevPlayerGUID = args.destName, args.destGUID
-		elseif prevPlayerName then
+		if not prevLinkGUID then
+			prevLinkName, prevLinkGUID = args.destName, args.destGUID
+		elseif prevLinkGUID then
 			if self:Me(args.destGUID) then
-				mySaySpamTarget = prevPlayerName
+				mySaySpamTarget = prevLinkName
 				if self:GetOption("custom_on_linked_spam") then
-					self:SimpleTimer(RepeatLinkSay, 3)
+					self:SimpleTimer(RepeatLinkSay, 5)
 				end
-				local msg = CL.link_with:format(prevPlayerName)
+				local msg = CL.link_with:format(prevLinkName)
 				self:PersonalMessage(args.spellId, false, msg)
-				self:Say(args.spellId, msg, true, ("Linked with %s"):format(prevPlayerName))
+				self:Say(args.spellId, msg, true, ("Linked with %s"):format(prevLinkName))
 				self:PlaySound(args.spellId, "warning", nil, args.destName)
-			elseif self:Me(prevPlayerGUID) then
+			elseif self:Me(prevLinkGUID) then
 				mySaySpamTarget = args.destName
 				if self:GetOption("custom_on_linked_spam") then
-					self:SimpleTimer(RepeatLinkSay, 3)
+					self:SimpleTimer(RepeatLinkSay, 5)
 				end
 				local msg = CL.link_with:format(args.destName)
 				self:PersonalMessage(args.spellId, false, msg)
 				self:Say(args.spellId, msg, true, ("Linked with %s"):format(args.destName))
-				self:PlaySound(args.spellId, "warning", nil, prevPlayerName)
+				self:PlaySound(args.spellId, "warning", nil, prevLinkName)
 			else
-				self:Message(args.spellId, "yellow", CL.link_both:format(self:ColorName(prevPlayerName), self:ColorName(args.destName)))
+				self:Message(args.spellId, "yellow", CL.link_both:format(self:ColorName(prevLinkName), self:ColorName(args.destName)))
 			end
-			prevPlayerName = nil
 		end
 	end
 
@@ -142,7 +154,7 @@ end
 do
 	local prev = 0
 	function mod:AmethystPoolDamage(args)
-		if self:Me(args.destGUID) and args.time - prev > 5 then
+		if self:Me(args.destGUID) and args.time - prev > 3 then
 			prev = args.time
 			self:PersonalMessage(args.spellId, "underyou")
 			self:PlaySound(args.spellId, "underyou")
@@ -152,25 +164,31 @@ end
 
 function mod:Petrifications(_, _, _, spellId)
 	if spellId == 115852 then -- Cobalt Petrification
+		currentPetri = 60051
 		self:Message(115852, "cyan")
 		self:PlaySound(115852, "alert")
 	elseif spellId == 116006 then -- Jade Petrification
+		currentPetri = 60043
 		self:Message(116006, "cyan")
 		self:PlaySound(116006, "alert")
 	elseif spellId == 116036 then -- Jasper Petrification
+		currentPetri = 59915
 		self:Message(116036, "cyan")
 		self:PlaySound(116036, "alert")
 	elseif spellId == 116057 then -- Amethyst Petrification
+		currentPetri = 60047
 		self:Message(116057, "cyan")
 		self:PlaySound(116057, "alert")
 	elseif spellId == 129424 then -- Cobalt Mine
-		self:Bar(129424, cobaltTimer)
+		self:StopBar(CL.count:format(self:SpellName(spellId), cobaltCount))
+		cobaltCount = cobaltCount + 1
+		self:CDBar(129424, cobaltTimer, CL.count:format(self:SpellName(spellId), cobaltCount))
 	end
 end
 
 function mod:EnergizedTilesApplied(args)
 	local amount = args.amount or 1
-	if amount ~= prevTiles then
+	if amount > prevTiles then
 		prevTiles = amount
 		if amount == 1 then
 			self:SetInfo("energy", 9, ("|T134457:0|t %s"):format(args.spellName))
@@ -186,6 +204,12 @@ do
 		["boss3"] = 5,
 		["boss4"] = 7,
 	}
+	local icons = {
+		[60051] = 134398, -- Cobalt Guardian
+		[60047] = 134399, -- Amethyst Guardian
+		[60043] = 134397, -- Jade Guardian
+		[59915] = 134396, -- Jasper Guardian
+	}
 	local prevPower = {
 		["boss1"] = 0,
 		["boss2"] = 0,
@@ -194,20 +218,34 @@ do
 	}
 	local prevSound = 0
 
-	function mod:UNIT_POWER_FREQUENT(_, unit)
+	function mod:UNIT_POWER_UPDATE(_, unit)
 		local power = UnitPower(unit)
-		if power ~= prevPower[unit] then
+		local prevPowerForUnit = prevPower[unit]
+		if power ~= prevPowerForUnit then
 			prevPower[unit] = power
-			local name = L[self:MobId(self:UnitGUID(unit))] or "??"
+			local npcID = self:MobId(self:UnitGUID(unit))
+			local name = L[npcID] or "??"
 			self:SetInfo("energy", bossList[unit], name)
 			self:SetInfo("energy", bossList[unit]+1, power)
-			self:SetInfoBar("energy", bossList[unit], power/100)
-			if power == 80 then
-				self:Message("energy", "red", CL.other:format(self:UnitName(unit), CL.energy_percent:format(power)), false)
-				local t = GetTime()
-				if t-prevSound > 2 then
-					prevSound = t
-					self:PlaySound("energy", "long")
+			if power >= 80 then
+				if npcID == currentPetri then
+					self:SetInfoBar("energy", bossList[unit], power/100, 0.5, 0, 1, 1) -- Always purple for petrified one
+				else
+					self:SetInfoBar("energy", bossList[unit], power/100, 1, 0, 0, 1) -- Red for danger level
+				end
+				if prevPowerForUnit < 80 then
+					self:Message("energy", "red", CL.other:format(self:UnitName(unit), CL.energy_percent:format(power)), icons[npcID] or false)
+					local t = GetTime()
+					if t-prevSound > 5 then
+						prevSound = t
+						self:PlaySound("energy", "long")
+					end
+				end
+			else
+				if npcID == currentPetri then
+					self:SetInfoBar("energy", bossList[unit], power/100, 0.5, 0, 1, 1) -- Always purple for petrified one
+				else
+					self:SetInfoBar("energy", bossList[unit], power/100)
 				end
 			end
 		end
